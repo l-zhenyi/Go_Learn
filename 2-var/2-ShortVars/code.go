c5ed4f506f8fc9ec029696f739a837af1318b3af
l-zhenyi <zhenyi.lim.mail@gmail.com>
2025-01-06 14:49:52 +0800

    Variables completed

---
+++ 2-var/2-ShortVars/code.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare here
+	congrats := "Happy birthday"
+
+	fmt.Println(congrats)
+}
---
+++ 2-var/8-FormatStrs/code.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	const name = "Saul Goodman"
+	const openRate = 30.5
+
+	// ?
+	msg := fmt.Sprintf(
+		"HI %s, your open rate is %.1f percent",
+		name, openRate,
+	)
+	// don't edit below this line
+
+	fmt.Println(msg)
+}
